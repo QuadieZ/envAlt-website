4841010670a0d9b388dbaa8cb28df5d90f885cad
QuadieZ <dduidokdoi@gmail.com>
2021-03-26 20:33:56 +0700

    "add files"

--- src/pages/dailyBranch.js
+++ src/pages/dailyBranch.js
@@ -7,14 +7,18 @@ const dailyBranch = () => {
     return (
         <Box backgroundColor="#C9F8F2" minHeight="100vh" minWidth="100%" position="absolute">
             <NavBar />
-            <VStack mt="18vh" mb="18vh" spacing={32}>
+            <VStack mt="18vh" mb="18vh" spacing={20}>
                 <VStack>
-                <Text fontSize="2xl">Electrical Appliances Performance of Branch : </Text>
-                <HStack justify="flex-end" width="100%" pr="15%">
-                    <Text fontSize="lg">Date: </Text>
-                   <DatePickerField/>
-                </HStack>
+                    <Text fontSize="2xl">Electrical Appliances Performance of Branch : </Text>
+                    <HStack justify="flex-end" width="100%" pr="15%">
+                        <Text fontSize="lg">Date: </Text>
+                        <DatePickerField />
+                    </HStack>
                 </VStack>
+                <VStack width="70vw" align="flex-start">
+                    <Text fontSize="xl">Customer Record</Text>
+                </VStack>
+
             </VStack>
         </Box>
     )
